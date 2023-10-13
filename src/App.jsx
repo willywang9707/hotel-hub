c4decf6e9b34639bc97c7fbd2b34c428e39744b8
willy <willywang9707@gmail.com>
2023-10-13 16:26:30 +0800

    feat(toast): integrate global toast notifications in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Toast from "./utils/Toast";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const notify = () => Toast.error("hello");
+    const notify2 = () => Toast.info("hello");
+    const notify3 = () => Toast.success("hello");
+    const notify4 = () => Toast.warning("hello");
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <div>
+            <button onClick={notify}>Notify!</button>
+            <button onClick={notify2}>Notify!</button>
+            <button onClick={notify3}>Notify!</button>
+            <button onClick={notify4}>Notify!</button>
+
+            <ToastContainer
+                position="bottom-right"
+                autoClose={5000}
+                hideProgressBar
+                newestOnTop
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="dark"
+            />
+        </div>
+    );
 }
 
-export default App
+export default App;
